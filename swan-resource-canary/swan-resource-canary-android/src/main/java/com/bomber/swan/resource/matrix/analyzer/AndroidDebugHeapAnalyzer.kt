a6d9e480be03b26c7b8245cec30d6f32ae9415a0
YoungTr <tangyan1004@gmail.com>
2022-04-22 18:19:55 +0800

    find activity fragment bitmap leaks

--- swan-resource-canary/swan-resource-canary-android/src/main/java/com/bomber/swan/resource/matrix/analyzer/AndroidDebugHeapAnalyzer.kt
+++ swan-resource-canary/swan-resource-canary-android/src/main/java/com/bomber/swan/resource/matrix/analyzer/AndroidDebugHeapAnalyzer.kt
@@ -177,19 +177,32 @@ object AndroidDebugHeapAnalyzer {
         //
         val leakingObjectIds = mutableSetOf<Long>()
         val leakReasonTable = mutableMapOf<Long, String>()
-        val activityLeakFilter = ActivityLeakFilter(
-            graph,
-            classObjectCounterMap,
-            leakingObjectIds,
-            leakReasonTable
-        )
 
-        val activityClass = graph.findClassByName(ACTIVITY_CLASS_NAME)
+        val activityHeapClass = graph.findClassByName(ACTIVITY_CLASS_NAME)
+        val fragmentHeapClass = graph.findClassByName(ANDROIDX_FRAGMENT_CLASS_NAME)
+        val bitmapHeapCass = graph.findClassByName(BITMAP_CLASS_NAME)
 
         for (instance in graph.instances) {
             if (instance.isPrimitiveWrapper) continue
 
-            val pair = superIdPair(instance, classHierarchyMap)
+            val instanceClassId = instance.instanceClassId
+
+            /**
+             * 使用HashMap缓存及遍历两边classHierarchy，这2种方式加速查找instance是否是对应类实例
+             * superId1代表类的继承层次中倒数第一的id，0就是继承自object
+             * superId4代表类的继承层次中倒数第四的id
+             * 类的继承关系，以AOSP代码为主，部分厂商入如OPPO Bitmap会做一些修改，这里先忽略
+             */
+            val (superId1, superId4) = if (classHierarchyMap[instanceClassId] != null) {
+                classHierarchyMap[instanceClassId]!!
+            } else {
+                val classHierarchyList = instance.instanceClass.classHierarchy.toList()
+                val first =
+                    classHierarchyList.getOrNull(classHierarchyList.size - 2)?.objectId ?: -1L
+                val second =
+                    classHierarchyList.getOrNull(classHierarchyList.size - 5)?.objectId ?: -1L
+                Pair(first, second).also { classHierarchyMap[instanceClassId] }
+            }
 
             /**
              * 遍历镜像所有class查找
@@ -216,24 +229,75 @@ object AndroidDebugHeapAnalyzer {
              * 1.Bitmap
              * 2.基本类型数组
              */
+            // Activity
+            if (activityHeapClass?.objectId == superId4) {
+                val destroyField = instance[ACTIVITY_CLASS_NAME, DESTROYED_FIELD_NAME]!!
+                val finishedField = instance[ACTIVITY_CLASS_NAME, FINISHED_FIELD_NAME]!!
+                if (destroyField.value.asBoolean!! || finishedField.value.asBoolean!!) {
+                    val objectCounter =
+                        updateClassObjectCounterMap(classObjectCounterMap, instanceClassId, true)
+                    SwanLog.i(
+                        TAG, "activity name : " + instance.instanceClassName
+                                + " mDestroyed:" + destroyField.value.asBoolean
+                                + " mFinished:" + finishedField.value.asBoolean
+                                + " objectId:" + (instance.objectId and 0xffffffffL)
+                    )
+                    if (objectCounter.leakCount <= SAME_CLASS_LEAK_OBJECT_PATH_THRESHOLD) {
+                        leakingObjectIds.add(instance.objectId)
+                        leakReasonTable[instance.objectId] = "Activity Leak"
+                    }
 
-//            val superId4 = pair.second
-//            if (activityClass?.objectId == superId4) {
-//                val destroyField = instance[ACTIVITY_CLASS_NAME, DESTROYED_FIELD_NAME]!!
-//                val finishedField = instance[ACTIVITY_CLASS_NAME, FINISHED_FIELD_NAME]!!
-//                if (destroyField.value.asBoolean!! || finishedField.value.asBoolean!!) {
-//                    SwanLog.i(
-//                        TAG, "activity name : " + instance.instanceClassName
-//                                + " mDestroyed:" + destroyField.value.asBoolean
-//                                + " mFinished:" + finishedField.value.asBoolean
-//                                + " objectId:" + (instance.objectId and 0xffffffffL)
-//                    )
-//
-//                }
-//            }
-            activityLeakFilter.findLeaking(instance, pair)
+                }
+                continue
+            }
+
+            // Fragment
+            if (fragmentHeapClass?.objectId == superId1) {
+                val className = fragmentHeapClass.name
+                SwanLog.d(TAG, "fragment class name: $className")
+                val fragmentManager = instance[className, FRAGMENT_MANAGER_FIELD_NAME]
+                if (fragmentManager != null && fragmentManager.value.asObject == null) {
+                    val mCalledField = instance[className, FRAGMENT_MCALLED_FIELD_NAME]
+                    val isLeak = mCalledField != null && mCalledField.value.asBoolean!!
+                    val objectCounter =
+                        updateClassObjectCounterMap(classObjectCounterMap, instanceClassId, isLeak)
+                    SwanLog.d(TAG, "fragment name: ${instance.instanceClassName} isLeak $isLeak")
+                    if (objectCounter.leakCount <= SAME_CLASS_LEAK_OBJECT_PATH_THRESHOLD && isLeak) {
+                        leakingObjectIds.add(instance.objectId)
+                        leakReasonTable[instance.objectId] = "Fragment leak"
+                    }
+                }
+
+                continue
+            }
+
+            // bitmap
+            if (bitmapHeapCass?.objectId == superId1) {
+                val fieldWidth = instance[BITMAP_CLASS_NAME, "mWidth"]
+                val fieldHeight = instance[BITMAP_CLASS_NAME, "mHeight"]
+
+                val with = fieldWidth!!.value.asInt!!
+                val height = fieldHeight!!.value.asInt!!
+
+                if (with * height > DEFAULT_BIG_BITMAP) {
+                    val objectCounter =
+                        updateClassObjectCounterMap(classObjectCounterMap, instanceClassId, true)
+                    SwanLog.d(
+                        TAG,
+                        "suspect leak! bitmap name :${instance.instanceClassName} width: $with height: $height"
+                    )
+                    if (objectCounter.leakCount < SAME_CLASS_LEAK_OBJECT_PATH_THRESHOLD) {
+                        leakingObjectIds.add(instance.objectId)
+                        leakReasonTable[instance.objectId] =
+                            "Bitmap size Over Threshold, $with x $height"
+                    }
+                }
+                continue
+            }
 
         }
+
+
 
         SwanLog.d(TAG, "analyze spend ${System.currentTimeMillis() - startTime} ms")
 
@@ -248,28 +312,6 @@ object AndroidDebugHeapAnalyzer {
             listOf()
         )
 
-    }
-
-    /**
-     * 使用HashMap缓存及遍历两边classHierarchy，这2种方式加速查找instance是否是对应类实例
-     * superId1代表类的继承层次中倒数第一的id，0就是继承自object
-     * superId4代表类的继承层次中倒数第四的id
-     * 类的继承关系，以AOSP代码为主，部分厂商入如OPPO Bitmap会做一些修改，这里先忽略
-     */
-    private fun superIdPair(
-        instance: HeapObject.HeapInstance,
-        classHierarchyMap: Map<Long, Pair<Long, Long>>
-    ): Pair<Long, Long> {
-        val instanceClassId = instance.instanceClassId
-        val pair = if (classHierarchyMap[instanceClassId] != null) {
-            classHierarchyMap[instanceClassId]!!
-        } else {
-            val classHierarchyList = instance.instanceClass.classHierarchy.toList()
-            val first = classHierarchyList.getOrNull(classHierarchyList.size - 2)?.objectId ?: -1L
-            val second = classHierarchyList.getOrNull(classHierarchyList.size - 5)?.objectId ?: -1L
-            Pair(first, second).also { classHierarchyMap[instanceClassId] }
-        }
-        return pair
     }
 
 
@@ -288,6 +330,24 @@ object AndroidDebugHeapAnalyzer {
         )
     }
 
+}
+
+private fun updateClassObjectCounterMap(
+    classObCountMap: MutableMap<Long, ObjectCounter>,
+    instanceClassId: Long,
+    isLeak: Boolean
+): ObjectCounter {
+    val objectCounter = classObCountMap[instanceClassId] ?: ObjectCounter().also {
+        classObCountMap[instanceClassId] = it
+    }
+
+    objectCounter.allCounter++
+
+    if (isLeak) {
+        objectCounter.leakCount++
+    }
+
+    return objectCounter
 }
 
 data class ObjectCounter(var allCounter: Int = 0, var leakCount: Int = 0)
