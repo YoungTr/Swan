eb4a62a1caf57806ce6cc365d76ebd380e17f6ce
YoungTr <tangyan1004@gmail.com>
2022-07-03 16:41:09 +0800

    suspend adapter 11

--- swan-resource-canary/swan-resource-canary-android/src/main/cpp/common/swan_dumper.c
+++ swan-resource-canary/swan-resource-canary-android/src/main/cpp/common/swan_dumper.c
@@ -2,10 +2,12 @@
 // Created by YoungTr on 2022/4/14.
 //
 
+#include <tls.h>
 #include <inttypes.h>
 #include <unistd.h>
 #include <sys/prctl.h>
 #include <sys/wait.h>
+#include <malloc.h>
 #include "log.h"
 #include "xdl.h"
 #include "swan_errno.h"
@@ -17,6 +19,17 @@ static int swan_dump_symbols_status = SWAN_ERRNO_NOTFND;
 
 static swan_libart_suspend dump_libart_suspend = NULL;
 static swan_libart_resumed dump_libart_resumed = NULL;
+
+static swan_libart_ssa_constructor_fnc_ dump_ssa_constructor = NULL;
+static swan_libart_ssa_destructor_fnc_ dump_ssa_destructor = NULL;
+static swan_libart_sgc_constructor_fnc_ dump_sgc_constructor = NULL;
+static swan_libart_sgc_destructor_fnc_ dump_sgc_destructor = NULL;
+static swan_libart_mutator_lock_ptr_ dump_mutator_lock_ptr = NULL;
+static swan_libart_exclusive_lock_fnc_ dump_exclusive_lock = NULL;
+static swan_libart_exclusive_unlock_fnc_ dump_exclusive_unlock = NULL;
+
+static void *swan_libart_ssa_instance;
+static void *swan_libart_sgc_instance;
 
 
 int swan_dump_init() {
@@ -33,13 +46,34 @@ int swan_dump_init() {
     LOGD(">>> xdl_open(%s) : handle %" PRIxPTR, "libart.so", (uintptr_t) handler);
 
     if (swan_common_api_level < __ANDROID_API_R__) { // < android 11
-        if (NULL == (dump_libart_resumed = xdl_sym(handler, SWAN_UTIL_LIBART_DBG_RESUME, NULL))) goto end;
-        if (NULL == (dump_libart_suspend = xdl_sym(handler, SWAN_UTIL_LIBART_DBG_SUSPEND, NULL))) goto end;
-        LOGD(">>> %s(%s) : addr %" PRIxPTR " ", "xdl_sym",
-             SWAN_UTIL_LIBART_DBG_RESUME, (uintptr_t) dump_libart_resumed);
+        if (NULL == (dump_libart_resumed = xdl_sym(handler, SWAN_DUMP_LIBART_DBG_RESUME, NULL))) goto end;
+        if (NULL == (dump_libart_suspend = xdl_sym(handler, SWAN_DUMP_LIBART_DBG_SUSPEND, NULL))) goto end;
+        LOGD(">>> %s(%s) : addr %" PRIxPTR " ", "xdl_sym", SWAN_DUMP_LIBART_DBG_RESUME, (uintptr_t) dump_libart_resumed);
+        LOGD(">>> %s(%s) : addr %" PRIxPTR " ", "xdl_sym", SWAN_DUMP_LIBART_DBG_SUSPEND, (uintptr_t) dump_libart_suspend);
+    } else if (swan_common_api_level <= __ANDROID_API_S__) {
+        if (NULL == (dump_sgc_destructor = xdl_sym(handler, SWAN_DUMP_LIBART_SGC_DESTRUCTOR, NULL))) goto end;
+        LOGD(">>> %s(%s) : addr %" PRIxPTR " ", "xdl_sym", SWAN_DUMP_LIBART_SGC_DESTRUCTOR, (uintptr_t) dump_sgc_destructor);
 
-        LOGD(">>> %s(%s) : addr %" PRIxPTR " ", "xdl_sym",
-             SWAN_UTIL_LIBART_DBG_SUSPEND, (uintptr_t) dump_libart_suspend);
+        if (NULL == (dump_ssa_constructor = xdl_sym(handler, SWAN_DUMP_LIBART_SSA_CONSTRUCTOR, NULL))) goto end;
+        LOGD(">>> %s(%s) : addr %" PRIxPTR " ", "xdl_sym", SWAN_DUMP_LIBART_SSA_CONSTRUCTOR, (uintptr_t) dump_ssa_constructor);
+
+        if (NULL == (dump_ssa_destructor = xdl_sym(handler, SWAN_DUMP_LIBART_SSA_DESTRUCTOR, NULL))) goto end;
+        LOGD(">>> %s(%s) : addr %" PRIxPTR " ", "xdl_sym", SWAN_DUMP_LIBART_SSA_DESTRUCTOR, (uintptr_t) dump_ssa_destructor);
+
+        if (NULL == (dump_mutator_lock_ptr = xdl_sym(handler, SWAN_DUMP_LIBART_MUTATOR_LOCK_PTR, NULL))) goto end;
+        LOGD(">>> %s(%s) : addr %" PRIxPTR " ", "xdl_sym", SWAN_DUMP_LIBART_MUTATOR_LOCK_PTR, (uintptr_t) dump_mutator_lock_ptr);
+
+        if (NULL == (dump_exclusive_lock = xdl_sym(handler, SWAN_DUMP_LIBART_EXCLUSIVE_LOCK, NULL))) goto end;
+        LOGD(">>> %s(%s) : addr %" PRIxPTR " ", "xdl_sym", SWAN_DUMP_LIBART_EXCLUSIVE_LOCK, (uintptr_t) dump_exclusive_lock);
+
+        if (NULL == (dump_exclusive_unlock = xdl_sym(handler, SWAN_DUMP_LIBART_EXCLUSIVE_UNLOCK, NULL))) goto end;
+        LOGD(">>> %s(%s) : addr %" PRIxPTR " ", "xdl_sym", SWAN_DUMP_LIBART_EXCLUSIVE_UNLOCK, (uintptr_t) dump_exclusive_unlock);
+
+        if (NULL == (dump_sgc_constructor = xdl_sym(handler, SWAN_DUMP_LIBART_SGC_CONSTRUCTOR, NULL))) goto end;
+        LOGD(">>> %s(%s) : addr %" PRIxPTR " ", "xdl_sym", SWAN_DUMP_LIBART_SGC_CONSTRUCTOR, (uintptr_t) dump_sgc_constructor);
+
+       swan_libart_sgc_instance = malloc(64);
+       swan_libart_ssa_instance = malloc(64);
     }
 
     swan_dump_symbols_status = 0;
@@ -52,6 +86,13 @@ pid_t swan_dump_suspend(int wait) {
     if (0 != swan_dump_symbols_status) return -1;
     if (swan_common_api_level < __ANDROID_API_R__) {
         dump_libart_suspend();
+    } else if (swan_common_api_level <= __ANDROID_API_S__) {
+        void *self = __get_tls()[TLS_SLOT_ART_THREAD_SELF];
+        dump_sgc_constructor(swan_libart_sgc_instance, self, kGcCauseHprof, kCollectorTypeHprof);
+        dump_ssa_constructor(swan_libart_ssa_instance, "HprofDump", 1);
+        // avoid deadlock with child process
+        dump_exclusive_unlock(*dump_mutator_lock_ptr, self);
+        dump_sgc_destructor(swan_libart_sgc_instance);
     }
 
     pid_t pid = fork();
@@ -68,6 +109,10 @@ int swan_dump_resumed(pid_t pid) {
 
     if (swan_common_api_level < __ANDROID_API_R__) {
         dump_libart_resumed();
+    } else if (swan_common_api_level <= __ANDROID_API_S__) {
+        void *self = __get_tls()[TLS_SLOT_ART_THREAD_SELF];
+        dump_exclusive_lock(*dump_mutator_lock_ptr, self);
+        dump_ssa_destructor(swan_libart_ssa_instance);
     }
 
     int status;
--- swan-resource-canary/swan-resource-canary-android/src/main/cpp/common/swan_dumper.h
+++ swan-resource-canary/swan-resource-canary-android/src/main/cpp/common/swan_dumper.h
@@ -11,12 +11,137 @@ extern "C" {
 
 #define SWAN_DUMP_LIB_ART ("libart.so")
 
-#define SWAN_UTIL_LIBART_DBG_SUSPEND      "_ZN3art3Dbg9SuspendVMEv"
-#define SWAN_UTIL_LIBART_DBG_RESUME       "_ZN3art3Dbg8ResumeVMEv"
+#define SWAN_DUMP_LIBART_DBG_SUSPEND      "_ZN3art3Dbg9SuspendVMEv"
+#define SWAN_DUMP_LIBART_DBG_RESUME       "_ZN3art3Dbg8ResumeVMEv"
+
+#define SWAN_DUMP_LIBART_SSA_CONSTRUCTOR        "_ZN3art16ScopedSuspendAllC1EPKcb"
+#define SWAN_DUMP_LIBART_SSA_DESTRUCTOR         "_ZN3art16ScopedSuspendAllD1Ev"
+#define SWAN_DUMP_LIBART_SGC_CONSTRUCTOR        "_ZN3art2gc23ScopedGCCriticalSectionC1EPNS_6ThreadENS0_7GcCauseENS0_13CollectorTypeE"
+#define SWAN_DUMP_LIBART_SGC_DESTRUCTOR         "_ZN3art2gc23ScopedGCCriticalSectionD1Ev"
+#define SWAN_DUMP_LIBART_MUTATOR_LOCK_PTR       "_ZN3art5Locks13mutator_lock_E"
+#define SWAN_DUMP_LIBART_EXCLUSIVE_LOCK         "_ZN3art17ReaderWriterMutex13ExclusiveLockEPNS_6ThreadE"
+#define SWAN_DUMP_LIBART_EXCLUSIVE_UNLOCK       "_ZN3art17ReaderWriterMutex15ExclusiveUnlockEPNS_6ThreadE"
+
 
 typedef void  (*swan_libart_suspend)(void);
 
 typedef void  (*swan_libart_resumed)(void);
+
+// What caused the GC?
+enum GcCause {
+    // Invalid GC cause used as a placeholder.
+    kGcCauseNone,
+    // GC triggered by a failed allocation. Thread doing allocation is blocked
+    // waiting for GC before
+    // retrying allocation.
+    kGcCauseForAlloc,
+    // A background GC trying to ensure there is free memory ahead of allocations.
+    kGcCauseBackground,
+    // An explicit System.gc() call.
+    kGcCauseExplicit,
+    // GC triggered for a native allocation when NativeAllocationGcWatermark is
+    // exceeded.
+    // (This may be a blocking GC depending on whether we run a non-concurrent
+    // collector).
+    kGcCauseForNativeAlloc,
+    // GC triggered for a collector transition.
+    kGcCauseCollectorTransition,
+    // Not a real GC cause, used when we disable moving GC (currently for
+    // GetPrimitiveArrayCritical).
+    kGcCauseDisableMovingGc,
+    // Not a real GC cause, used when we trim the heap.
+    kGcCauseTrim,
+    // Not a real GC cause, used to implement exclusion between GC and
+    // instrumentation.
+    kGcCauseInstrumentation,
+    // Not a real GC cause, used to add or remove app image spaces.
+    kGcCauseAddRemoveAppImageSpace,
+    // Not a real GC cause, used to implement exclusion between GC and debugger.
+    kGcCauseDebugger,
+    // GC triggered for background transition when both foreground and background
+    // collector are CMS.
+    kGcCauseHomogeneousSpaceCompact,
+    // Class linker cause, used to guard filling art methods with special values.
+    kGcCauseClassLinker,
+    // Not a real GC cause, used to implement exclusion between code cache
+    // metadata and GC.
+    kGcCauseJitCodeCache,
+    // Not a real GC cause, used to add or remove system-weak holders.
+    kGcCauseAddRemoveSystemWeakHolder,
+    // Not a real GC cause, used to prevent hprof running in the middle of GC.
+    kGcCauseHprof,
+    // Not a real GC cause, used to prevent GetObjectsAllocated running in the
+    // middle of GC.
+    kGcCauseGetObjectsAllocated,
+    // GC cause for the profile saver.
+    kGcCauseProfileSaver,
+    // GC cause for running an empty checkpoint.
+    kGcCauseRunEmptyCheckpoint,
+};
+
+// Which types of collections are able to be performed.
+enum CollectorType {
+    // No collector selected.
+    kCollectorTypeNone,
+    // Non concurrent mark-sweep.
+    kCollectorTypeMS,
+    // Concurrent mark-sweep.
+    kCollectorTypeCMS,
+    // Semi-space / mark-sweep hybrid, enables compaction.
+    kCollectorTypeSS,
+    // Heap trimming collector, doesn't do any actual collecting.
+    kCollectorTypeHeapTrim,
+    // A (mostly) concurrent copying collector.
+    kCollectorTypeCC,
+    // The background compaction of the concurrent copying collector.
+    kCollectorTypeCCBackground,
+    // Instrumentation critical section fake collector.
+    kCollectorTypeInstrumentation,
+    // Fake collector for adding or removing application image spaces.
+    kCollectorTypeAddRemoveAppImageSpace,
+    // Fake collector used to implement exclusion between GC and debugger.
+    kCollectorTypeDebugger,
+    // A homogeneous space compaction collector used in background transition
+    // when both foreground and background collector are CMS.
+    kCollectorTypeHomogeneousSpaceCompact,
+    // Class linker fake collector.
+    kCollectorTypeClassLinker,
+    // JIT Code cache fake collector.
+    kCollectorTypeJitCodeCache,
+    // Hprof fake collector.
+    kCollectorTypeHprof,
+    // Fake collector for installing/removing a system-weak holder.
+    kCollectorTypeAddRemoveSystemWeakHolder,
+    // Fake collector type for GetObjectsAllocated
+    kCollectorTypeGetObjectsAllocated,
+    // Fake collector type for ScopedGCCriticalSection
+    kCollectorTypeCriticalSection,
+};
+
+/**
+  * Function pointer for ART Android R
+  */
+// art::ScopedSuspendAll::ScopedSuspendAll()
+typedef void (*swan_libart_ssa_constructor_fnc_)(void *handle, const char *cause, int long_suspend);
+
+// art::ScopedSuspendAll::~ScopedSuspendAll()
+typedef void (*swan_libart_ssa_destructor_fnc_)(void *handle);
+
+// art::gc::ScopedGCCriticalSection::ScopedGCCriticalSection()
+typedef void (*swan_libart_sgc_constructor_fnc_)(void *handle, void *self, enum GcCause,  enum CollectorType);
+
+// art::gc::ScopedGCCriticalSection::~ScopedGCCriticalSection()
+typedef void (*swan_libart_sgc_destructor_fnc_)(void *handle);
+
+// art::Locks::mutator_lock_
+typedef void **swan_libart_mutator_lock_ptr_;
+
+// art::ReaderWriterMutex::ExclusiveLock
+typedef void (*swan_libart_exclusive_lock_fnc_)(void *, void *self);
+
+// art::ReaderWriterMutex::ExclusiveUnlock
+typedef void (*swan_libart_exclusive_unlock_fnc_)(void *, void *self);
+
 
 int swan_dump_init();
 
